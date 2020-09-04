f5316984be6d8bbc29f2c17c7403a989769c2a0d
anikal2001 <anikal2001@gmail.com>
2020-09-04 14:32:15 -0400

    First Commit

---
+++ src/components/header.js
@@ -0,0 +1,68 @@
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton"
+import { Link } from "gatsby"
+import MenuIcon from "@material-ui/icons/Menu"
+import { Dropdown } from 'react-bootstrap'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
+
+export default function ButtonAppBar() {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <Button color="inherit"> Home</Button>
+          </Link>{" "}
+          <br />
+          <Link to="/page-2/" style={{ textDecoration: "none" }}>
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Dropdown Button
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Link>
+          <br />
+          <Link to="/page-2/" style={{ textDecoration: "none" }}>
+            <Button color="inherit"> About Us</Button>
+          </Link>{" "}
+          <br />
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
